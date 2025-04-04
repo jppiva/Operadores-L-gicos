ba4609e9f1f06762ae38433d5a7884531e735f7d
joao pedro piva <joaopedropiva17@gmail.com>
2025-04-04 07:54:32 -0300

    sauvar

---
+++ Operadores Lógicos - Verdade ou mentira_1.js
@@ -0,0 +1,6 @@
+let numero1 = -10
+if (numero1 < 0){console.log('negativo')}
+else {console.log('positivo')}
+let numero2 = -20
+if (numero2 < 0){console.log('negativo')}
+else {console.log('positivo')}
