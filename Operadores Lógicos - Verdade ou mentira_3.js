b17c323608f990333040eee73ff7888b1b322651
joao pedro piva <joaopedropiva17@gmail.com>
2025-04-04 08:14:22 -0300

    sauvar

---
+++ Operadores Lógicos - Verdade ou mentira_3.js
@@ -0,0 +1,7 @@
+let numero1 = 20
+let numero2 = 79
+let numero3 = 101
+if (numero3 > 100 || numero1 > 100 || numero2 > 100) {
+    console.log('um numeros passa de 100 ')
+}
+else { console.log('nenhum numero passa de 100') }
